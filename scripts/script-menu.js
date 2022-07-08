dd7b8e69417f26f25d1be78a7b04b22b9982f3c9
Gustavo Goulart <66621683+gosttavo@users.noreply.github.com>
2022-07-08 00:05:17 -0300

    Add files via upload

---
+++ scripts/script-menu.js
@@ -0,0 +1,9 @@
+const btnMobile = document.querySelector("#btn-menu");
+
+function toggleMenu(){
+    const btnMobile = document.querySelector("nav");
+
+    nav.classList.toggle('active')
+}
+
+btnMobile.addEventListener('click', toggleMenu)
